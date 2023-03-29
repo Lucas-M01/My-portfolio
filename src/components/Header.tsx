185b65dbda6213e23acbf1bece1432324ee7268d
Lucas-M01 <lucas.montenegro.n@outlook.com>
2023-03-28 22:25:32 -0300

    Deixando o componente Header responsivo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,56 @@
+import Hamburger from 'hamburger-react'
+import { useState } from 'react'
+
 export function Header() {
+  const [openNav, setOpenNav] = useState(false)
+
   return (
-    <header className="flex justify-between fixed top-0 left-0 w-full py-8 px-[9%] bg-gray-800 text-gray-200 items-center z-[100]">
-      <a className="cursor-default font-semibold text-[2rem]" href="#">
-        Portfolio
-      </a>
-      <nav className="text-[1.7rem] ml-16 flex gap-8 transition ">
-        <a className="hover:text-yellow-500 duration-300" href="#home">
-          Home
-        </a>
-        <a className="hover:text-yellow-500 duration-300" href="#about">
-          About
-        </a>
-        <a className="hover:text-yellow-500 duration-300" href="#portfolio">
-          Portfolio
-        </a>
-        <a className="hover:text-yellow-500 duration-300" href="#contact">
-          Contact
-        </a>
+    <header className="w-full py-5 md:py-8 px-[9%] bg-gray-800 text-gray-200 relative">
+      <nav className="text-xl md:text-2xl w-full md:ml-16">
+        <div className="text-center px-[1em] py-0">
+          <a
+            className="cursor-default font-semibold text-2xl md:text-[2rem]"
+            href="#"
+          >
+            Portfolio
+          </a>
+        </div>
+
+        <ul
+          className={`text-gray-200 bg-gray-700 text-center transition w-full h-screen flex flex-col justify-evenly fixed top-0 ${
+            openNav === false ? 'left-full' : 'left-0'
+          }`}
+        >
+          <li>
+            <a className="hover:text-yellow-500 duration-300" href="#home">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="hover:text-yellow-500 duration-300" href="#about">
+              About
+            </a>
+          </li>
+          <li>
+            <a className="hover:text-yellow-500 duration-300" href="#portfolio">
+              Portfolio
+            </a>
+          </li>
+          <li>
+            <a className="hover:text-yellow-500 duration-300" href="#contact">
+              Contact
+            </a>
+          </li>
+        </ul>
       </nav>
+      <div className="block cursor-pointer translate-y-[-50%] absolute top-1/2 right-4 z-[4]">
+        <Hamburger
+          size={25}
+          toggled={openNav}
+          toggle={setOpenNav}
+          duration={0.5}
+        />
+      </div>
     </header>
   )
 }
